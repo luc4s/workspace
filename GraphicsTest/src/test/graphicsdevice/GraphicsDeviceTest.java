49b54fe26007bcfe210ec746d2b3d162e83fdb9a
K4su <swiiss32@gmail.com>
2015-01-29 17:38:10 +0100

    Working example for a DisplayMode selection. Still buggy though..?

--- GraphicsTest/src/test/graphicsdevice/GraphicsDeviceTest.java
+++ GraphicsTest/src/test/graphicsdevice/GraphicsDeviceTest.java
@@ -2,18 +2,17 @@ package test.graphicsdevice;
 
 import java.awt.BorderLayout;
 import java.awt.DisplayMode;
+import java.awt.Font;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
-import javax.swing.AbstractListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JScrollPane;
 import javax.swing.ListSelectionModel;
@@ -27,51 +26,46 @@ public final class GraphicsDeviceTest {
         final GraphicsEnvironment graphicsEnv = GraphicsEnvironment.getLocalGraphicsEnvironment();
         final GraphicsDevice graphicsDev = graphicsEnv.getDefaultScreenDevice();
         
-        
         final JFrame frame = new JFrame("Resolution Selection");
           frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
           
-          JList<String> displayModeList = new JList<String>();
-            displayModeList.setModel(new AbstractListModel<String>() {
-              private static final long serialVersionUID = 1L;
-              
-              final List<String> displayModes = new ArrayList<String>();
-              {
-                final Set<String> dm = new HashSet<String>();
-                for (final DisplayMode displayMode : graphicsDev.getDisplayModes())
-                  dm.add(String.format("%4dx%-4d [%dbit]",
-                    displayMode.getWidth(), displayMode.getHeight(), displayMode.getBitDepth()));
-                for (final String displayMode : dm)
-                  displayModes.add(displayMode);
-              }
-              
-              public int getSize() {
-                return displayModes.size();
-              }
-  
-              @Override
-              public String getElementAt(int index) {
-                return displayModes.get(index);
-              }
-            });
+          final List<String> displayModeStr = new ArrayList<String>();
+          for (final DisplayMode displayMode : graphicsDev.getDisplayModes())
+            displayModeStr.add(
+              String.format("%4dx%-4d [%2dbit|%2dHz]",
+                displayMode.getWidth(), displayMode.getHeight(), displayMode.getBitDepth(), displayMode.getRefreshRate()));
+          final JList<String> displayModeList = new JList<String>(displayModeStr.toArray(new String[0]));
             displayModeList.setVisibleRowCount(4);
             displayModeList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
             displayModeList.setSelectedIndex(0);
           
-          final JScrollPane scrollPane = new JScrollPane(displayModeList);
+          final JScrollPane scrollList = new JScrollPane(displayModeList);
           
-          final JButton submit = new JButton("Launch");
-            submit.addActionListener(new ActionListener() {
+          final JButton submitButton = new JButton("Launch");
+            submitButton.addActionListener(new ActionListener() {
               @Override
-              public void actionPerformed(ActionEvent e) {
-                final String displayModeStr = displayModeList.getSelectedValue();
-                final DisplayMode displayMode = new DisplayMode;
+              public void actionPerformed(final ActionEvent evt) {
+                final DisplayMode displayMode = graphicsDev.getDisplayModes()[displayModeList.getSelectedIndex()];
+                
+                frame.dispose();
+                
+                final JFrame fullscreen = new JFrame("Fullscreen");
+                  fullscreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                  fullscreen.setUndecorated(true);
+                  final JLabel hello = new JLabel("Hello C:");
+                    hello.setFont(new Font("Helvetica", Font.BOLD, 30));
+                  
+                  fullscreen.getContentPane().setLayout(new BorderLayout());
+                  fullscreen.getContentPane().add(hello, BorderLayout.CENTER);
+                
+                graphicsDev.setFullScreenWindow(fullscreen);
+                graphicsDev.setDisplayMode(displayMode);
               }
             });
           
           frame.getContentPane().setLayout(new BorderLayout());
-          frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
-          frame.getContentPane().add(submit, BorderLayout.SOUTH);
+          frame.getContentPane().add(scrollList, BorderLayout.CENTER);
+          frame.getContentPane().add(submitButton, BorderLayout.SOUTH);
           
           frame.setResizable(false);
           frame.pack();
